69c2fa846823515d030efea90aeb569520e2e002
Bilaldem0 <224212023@ogrenci.amasya.edu.tr>
2023-11-20 20:57:03 +0300

    kutuphaneyi kullanma
    open cvnin önceden eğitilmiş sınıfını kullanarak yüz algılamayı gerçekleştirdim

--- envproje/camera.py
+++ envproje/camera.py
@@ -4,20 +4,24 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 
-if not cap.isOpened():
-    print("Kamera açılamadı.")
-    exit()
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
 
     ret, frame = cap.read()
 
-    if not ret:
-        print("Kare alınamadı.")
-        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
-    cv2.imshow("Kamera Görüntüsü", frame)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+    cv2.imshow('Face Detection', frame)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
